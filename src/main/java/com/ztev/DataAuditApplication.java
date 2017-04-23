ffe038c9c21584fd287a874edc72ef3044e4ac5b
christ-shaw <christ_shaw@hotails.com>
2017-04-23 19:25:14 +0800

    data audit commit

---
+++ src/main/java/com/ztev/DataAuditApplication.java
@@ -0,0 +1,12 @@
+package com.ztev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataAuditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataAuditApplication.class, args);
+	}
+}
